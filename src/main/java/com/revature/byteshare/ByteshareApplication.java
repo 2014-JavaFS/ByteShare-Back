fd1eb6b8160d5ae224dba6199ecb4ae1aa72c750
aramsing <aramsin2@hotmail.com>
2024-08-13 16:25:30 -0500

    first commit

---
+++ src/main/java/com/revature/byteshare/ByteshareApplication.java
@@ -0,0 +1,13 @@
+package com.revature.byteshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteshareApplication.class, args);
+	}
+
+}
