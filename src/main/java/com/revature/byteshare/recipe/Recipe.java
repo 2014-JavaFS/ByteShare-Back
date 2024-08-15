7c14b1bf91f83cb9d832fa3fe8f95dbede003961
rubensamf <ruben146@revature.net>
2024-08-15 16:11:36 -0500

    Added author, date, content, prepTime, and cookTime fields that somehow went missing when merging branches

--- src/main/java/com/revature/byteshare/recipe/Recipe.java
+++ src/main/java/com/revature/byteshare/recipe/Recipe.java
@@ -1,13 +1,13 @@
 package com.revature.byteshare.Recipe;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.revature.byteshare.User.User;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
 
 @Entity
 @Data
@@ -19,4 +19,15 @@ public class Recipe {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User author;
+
+    @Column(name = "timestamp", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private Timestamp date;
+
+    // TODO: Spring annotations for these fields
+    private String content; //either blob or text in SQL
+    private int prepTime; // in minutes
+    private int cookTime; // in minutes
 }
