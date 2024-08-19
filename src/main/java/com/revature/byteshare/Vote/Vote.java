a11fec1bd0d32dd1f49593ebfe19f4793f3f5539
KayoJules <ChaoticPhoenix777@gmail.com>
2024-08-19 10:05:14 -0500

    Added imports

--- src/main/java/com/revature/byteshare/Vote/Vote.java
+++ src/main/java/com/revature/byteshare/Vote/Vote.java
@@ -8,6 +8,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import com.revature.byteshare.recipe.Recipe;
+import com.revature.byteshare.user.User;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -18,8 +21,8 @@ public class Vote {
     @Id
     @GeneratedValue
     private int vote_id;
-    // private User voter;
-    // private Post post;      // Post or Recipe?
+    private User voter;
+    private Recipe recipe;      // Post or Recipe?
     private boolean isUpvote;
 }
 
---
+++ src/test/java/com/revature/byteshare/Vote/VoteRepositoryTest.java
@@ -0,0 +1,4 @@
+package com.revature.byteshare.Vote;
+
+public class VoteRepositoryTest {
+}
