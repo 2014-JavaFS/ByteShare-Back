f8b7f980da51e392916478e25e5033d2bf361dbd
aramsing <aramsin2@hotmail.com>
2024-08-28 09:35:10 -0500

    cross origin correction

--- src/main/java/com/revature/byteshare/user/UserController.java
+++ src/main/java/com/revature/byteshare/user/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.security.sasl.AuthenticationException;
 import java.util.List;
 
-@CrossOrigin
+
 @RestController
 @RequestMapping("/users")
 @CrossOrigin
